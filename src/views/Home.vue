8915e1ac5725f19fad997b2662047c36de3da430
StefanyRepetcki <stefany.c.r@hotmail.com>
2022-02-07 19:50:34 -0300

    feat(Home): importacao do componente calculadora

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculadora />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Calculadora from '@/components/Calculadora.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Calculadora
   }
 }
 </script>
